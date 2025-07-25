733c4ab767ad806e825f420d01780615db62ab64
haseeb <haseeeb8j@gmail.com>
2025-07-25 22:41:45 +0500

    improved: Error handling while loading model classes

--- sapphire/model/manager.py
+++ sapphire/model/manager.py
@@ -33,10 +33,12 @@ class ModelManager(SapphireModule):
 	def name(cls) -> str:
 		return "model"
 	
+
 	def handled_events(self) -> list[type[SapphireEvents.Event]]:
 		return [
 			SapphireEvents.PromptEvent
 		]
+
 
 	def handle(self, event: SapphireEvents.Event) -> None:
 
@@ -58,15 +60,20 @@ class ModelManager(SapphireModule):
 
 
 	def init_models(self):
+		"""
+		Search and Initialize all model providers.
+		All python modules within the model directories are searched for a get_model() 
+		method.
+		"""
 
 		model_mods = self.import_models()
 		
 		for mod in model_mods:
 
-			if not hasattr(mod, "get_model"):
-				continue
-			
-			model_class = mod.get_model()
+			model_class = self.get_model_from_module(mod)
+
+			if not model_class: continue
+
 			model: BaseModelProvider = model_class(self.config.get_sub_config(model_class.name()))
 			self.registered_providers[model.name()] = model
 
@@ -78,18 +85,28 @@ class ModelManager(SapphireModule):
 
 
 	def import_models(self) -> list[ModuleType]:
+		"""
+		Get all model classes returned by get_model() of all python modules within models/
+		"""
 	
 		mods = []
+
 		for sub in self.model_directory.iterdir():
+
 			if not sub.is_dir():
 				continue
 			 
 			mod_init = sub / "__init__.py"
 			
 			if not mod_init.exists():
-				pass #Error here
+				self.log(
+					SapphireEvents.chain(),
+					"debug",
+					f"Ignoring {sub.absolute()}. Not a python module."
+				)
 
 			mod_name = f"models.{sub.name}"
+
 			spec = importlib.util.spec_from_file_location(mod_name, mod_init)
 
 			if not spec: continue
@@ -97,40 +114,50 @@ class ModelManager(SapphireModule):
 
 			mod = importlib.util.module_from_spec(spec)
 			spec.loader.exec_module(mod)
-
 			mods.append(mod)
 		
 		return mods
-			
-
-
-	def generate_response(self, event: SapphireEvents.PromptEvent):
-		response = self.current_model.generate(event)
-
-		if response is not None:
-			
-			extras = {}
-			for ex in response.extras:
-				extras[ex.key] = ex.value
-			
-			ai_msg = SapphireEvents.AIResponseEvent(
-				self.name(),
-				SapphireEvents.make_timestamp(),
-				SapphireEvents.chain(event),
-				response.message,
-				extras
-			)
-
-			self.emit_event(ai_msg)
-
-			return
-
-		self.log(
-				SapphireEvents.chain(event),
-				"critical",
-				f"Could not get response from model '{self.current_model.name()}'"
-			)
 	
+
+	def get_model_from_module(self, mod: ModuleType) -> type[BaseModelProvider] | None:
+
+		if not hasattr(mod, "get_model"):
+			self.log(
+				SapphireEvents.chain(),
+				"warning",
+				f"A python module found in model/: '{mod.__name__}' has no get_model method"
+			)
+			return None
+
+		model_cls = mod.get_model()
+
+		if not issubclass(model_cls, BaseModelProvider):
+			self.log(
+				SapphireEvents.chain(),
+				"critical",
+				f"A python module found in model/: '{mod.__name__}' returned an invalid model provider. " \
+				f"Expected BaseModelProvider, Got {type(model_cls)}"
+			)
+			return None
+		
+		name = model_cls.name()
+
+		if name in self.registered_providers.keys():
+			self.log(
+				SapphireEvents.chain(),
+				"critical",
+				f"A python module found in model/: '{mod.__name__}' returned an invalid model provider. " \
+				f"Model tried to register with name '{name}' but it already exists." \
+				"Could lead to security risks in case of API key transfers. Raising Error."
+			)
+
+
+			return None
+		
+		return model_cls
+
+
+			
 
 	def load_model(self, model: str) -> None:
 
@@ -163,3 +190,33 @@ class ModelManager(SapphireModule):
 			"info",
 			f"Loaded model '{model}'."
 		)	
+
+	
+	def generate_response(self, event: SapphireEvents.PromptEvent):
+		response = self.current_model.generate(event)
+
+		if response is not None:
+			
+			extras = {}
+			for ex in response.extras:
+				extras[ex.key] = ex.value
+			
+			ai_msg = SapphireEvents.AIResponseEvent(
+				self.name(),
+				SapphireEvents.make_timestamp(),
+				SapphireEvents.chain(event),
+				response.message,
+				extras
+			)
+
+			self.emit_event(ai_msg)
+
+			return
+
+		self.log(
+				SapphireEvents.chain(event),
+				"critical",
+				f"Could not get response from model '{self.current_model.name()}'"
+			)
+	
+
