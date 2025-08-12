4a036407a3e33af87314b61d2e8d3ab8a3a6a066
haseeb <haseeeb8j@gmail.com>
2025-08-12 21:56:52 +0500

    changed task calling

--- modules/filesystem/filesystem.py
+++ modules/filesystem/filesystem.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 from typing import Tuple
+from pathlib import Path
 from sapphire.base import SapphireEvents, SapphireModule, SapphireConfig, SapphireTask
 
 
@@ -10,20 +11,44 @@ class FileSystem(SapphireModule):
 		super().__init__(emit_event, config)
 		self.has_tasks = True
 
+
 	@classmethod
 	def name(cls):
 		return "filesystem"
 	
+
 	def handled_events(self) -> list[type[SapphireEvents.Event]]:
 		return []
 	
+
 	def start(self) -> None:
 		pass
 
+
 	def end(self) -> Tuple[bool, str]:
-		return (False, "testing!")
+		return (True, "")
 	
-	@SapphireTask("read_file", "Read the contents of a file.", ["path:str"])
-	def read_file(self, args: list[str], chain: SapphireEvents.Chain):
-		with open(args[0]) as file:
-			return file.read()
+
+	@SapphireTask("read_file", "Read the contents of a file.", ["path:str", "lines:int"])
+	def read_file(self, chain: SapphireEvents.chain, path_raw: str, lines: str):
+
+		path = Path(path_raw)
+		lines = int(lines) if lines.isdigit() else 10
+
+		with open(path) as file:
+			content = file.readlines(lines)
+
+		return "".join(content)
+
+
+	@SapphireTask("list_directory", "List the contents of a directory/folder.", ["path:str"])
+	def list_directory(self, chain: SapphireEvents.Chain, path_raw: str):
+
+		path = Path(path_raw)
+
+		if not path.is_dir():
+			raise ValueError(f"{path} is not a directory!")
+
+		contents = [item.name for item in path.iterdir()]
+
+		return contents
