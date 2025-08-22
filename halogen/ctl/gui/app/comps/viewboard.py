7883b155766664971c59ee8613e5d269aec0b212
haseeb <haseeeb8j@gmail.com>
2025-08-22 21:36:36 +0500

    Changed components + Readded TUI

--- halogen/ctl/gui/app/comps/viewboard.py
+++ halogen/ctl/gui/app/comps/viewboard.py
@@ -3,12 +3,12 @@ from PySide6.QtWidgets import (
 	QVBoxLayout,
 	QScrollArea
 )
-
+from PySide6.QtCore import QThread
 
 from halogen.modules.server import HalogenInterface
 
 class ViewBoard(QWidget):
 
 	def __init__(self, interface: HalogenInterface):
-
-		self.interface = interfa
+		self.interface = interface
+		self.view = QScrollArea()
