243ab449decc3af67f5716d41c3ba99e1e26bbb1
haseeb <haseeeb8j@gmail.com>
2025-08-28 19:13:36 +0500

    Added from directory loading to PyModuleLoader

--- halogen/base/helpers.py
+++ halogen/base/helpers.py
@@ -1,7 +1,8 @@
-import importlib
+import importlib, sys
 from importlib import util
 from pathlib import Path
 from types import ModuleType
+
 
 
 class PyModuleLoader():
@@ -18,7 +19,10 @@ class PyModuleLoader():
 	def __init__(self):
 		pass
 
-	def from_file(self, file_path: Path) -> ModuleType:
+	def from_file(self, name: str, file_path: Path | str) -> ModuleType:
+		"Loading module from a file. Raises PyModuleError on any failure."
+		
+		if type(file_path) is str: file_path = Path(file_path)
 
 		if not file_path.exists():
 			raise PyModuleLoader.PyModuleError(f"{file_path.absolute()} does not exist.")
@@ -26,16 +30,32 @@ class PyModuleLoader():
 		if not file_path.is_file():
 			raise PyModuleLoader.PyModuleError(f"{file_path.absolute()} is not a file!")
 
-		mod_name = file_path.name.split(".")[0]
 
-		spec = util.spec_from_file_location(mod_name, file_path)
+		spec = util.spec_from_file_location(name, file_path)
 
-		if not spec or spec.loader:
+		if not spec or not spec.loader:
 			raise PyModuleLoader.PyModuleError(
-				f"Could not load spec and spec.loader for module at {file_path.absolute()}"
+				f"Could not load spec and spec.loader for module from {file_path.absolute()}."
 				)
-			
 
-		
-		
-		
+		mod = util.module_from_spec(spec)
+		sys.modules[name] = mod
+
+		try:
+			spec.loader.exec_module(mod)
+		except Exception as e:
+			raise PyModuleLoader.PyModuleError(
+				f"Failed to execute module from {file_path.absolute()}."
+				) from e
+
+		return mod
+
+	
+	def from_directory(self, name: str, dir_path: Path | str) -> ModuleType:
+		"Loading a module from a directory. __init__.py must be present."
+
+		if type(dir_path) is str: dir_path = Path(dir_path)
+
+		file_path = dir_path / "__init__.py"
+		mod = self.from_file(name, file_path)
+		return mod
